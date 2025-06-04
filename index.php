0cb797932f1db51da70e3407ff8c00baddd74bd9
Mazi.Tech <167058508+Mahziyar-Alzadeh@users.noreply.github.com>
2025-06-04 07:44:44 +0330

    normal for test

--- index.php
+++ index.php
@@ -1,14 +1,3 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>فرم ثبت نام با دیتابیس</title>
-</head>
-<body>
-    <h1>فرم ثبت نام</h1>
-    <form method="post" action="">
-        <label>نام شما:</label><br>
-        <input type="text" name="name" required><br><br>
-        <button type="submit">ارسال</button>
-    </form>
-</body>
-</html>
+<?php
+echo "my backend for testing";
+?>
