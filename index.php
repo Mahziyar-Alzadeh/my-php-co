8544d65ec5d3df4094c955ca6a754f9c68956df1
Mazi.Tech <167058508+Mahziyar-Alzadeh@users.noreply.github.com>
2025-06-04 06:52:04 +0330

    normal for test

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>فرم ثبت نام با دیتابیس</title>
+</head>
+<body>
+    <h1>فرم ثبت نام</h1>
+    <form method="post" action="">
+        <label>نام شما:</label><br>
+        <input type="text" name="name" required><br><br>
+        <button type="submit">ارسال</button>
+    </form>
+</body>
+</html>
